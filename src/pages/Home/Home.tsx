23433909d04d69f5ce7b6d9037a2cfe678914117
nmmn1004 <jjj4120@gmail.com>
2026-02-22 15:02:20 +0900

    Fix: path

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 
 import Logo from '../../assets/Logo.svg'
 import Download from '../../assets/Download.png'
-import App from '../../../public/app/toktory.apk'
 import Banner from '../../assets/main-banner/banner1.png'
 import { EXAMPLE_LIST, ExampleItem } from '../../assets/example';
 
@@ -15,7 +14,7 @@ function Home() {
       <header className="home-header-container">
         <img src={Logo} alt="TokTory Logo" className="home-header-logo-img" />
         <span className="home-header-title">TokTory</span>
-        <a href={App} download="toktory.apk">
+        <a href="../../../public/app/toktory.apk" download="toktory.apk">
           <img src={Download} className="home-header-download" />
         </a>
       </header>
